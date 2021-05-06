367991678e59f53c7a9ed591c9d437b61713dd84
akio0911 <akio0911@gmail.com>
2021-05-06 18:59:51 +0900

    presentAlertを追加

--- practice5/ViewController.swift
+++ practice5/ViewController.swift
@@ -14,24 +14,11 @@ class ViewController: UIViewController {
 
     @IBAction private func calucrateButton(_ sender: Any) {
         if devidedNumber.text == ""{
-            let alert: UIAlertController = UIAlertController(title: "課題5", message: "割られる数を入力してください", preferredStyle: UIAlertController.Style.alert)
-            let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (_: UIAlertAction!) -> Void in
-            })
-            alert.addAction(defaultAction)
-            present(alert, animated: true, completion: nil)
+            presentAlert(message: "割られる数を入力してください")
         } else if devideNumber.text == "" {
-            let alert: UIAlertController = UIAlertController(title: "課題5", message: "割る数を入力してください", preferredStyle: UIAlertController.Style.alert)
-            let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (_: UIAlertAction!) -> Void in
-            })
-            alert.addAction(defaultAction)
-            present(alert, animated: true, completion: nil)
+            presentAlert(message: "割る数を入力してください")
         } else if devideNumber.text == "0" {
-            let alert: UIAlertController = UIAlertController(title: "課題5", message: "0以外を入力してください", preferredStyle: UIAlertController.Style.alert)
-
-            let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (_: UIAlertAction!) -> Void in
-            })
-            alert.addAction(defaultAction)
-            present(alert, animated: true, completion: nil)
+            presentAlert(message: "0以外を入力してください")
         } else {
             var result: Double = 1.0
             let devide = Double(String(devideNumber.text ?? "")) ?? 0
@@ -39,5 +26,14 @@ class ViewController: UIViewController {
             result = devided / devide
             resultNumber.text = String(result)
         }
+    }
+
+    private func presentAlert(message: String) {
+        let alert: UIAlertController = UIAlertController(title: "課題5", message: message, preferredStyle: UIAlertController.Style.alert)
+
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (_: UIAlertAction!) -> Void in
+        })
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
     }
 }
